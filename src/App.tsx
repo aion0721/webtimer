45d7f437cf85884de24e65e38c78d9465c13cb8f
aion0721 <rudolf.aion@gmail.com>
2025-06-29 21:21:54 +0900

    fix: remove unuse file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import "./App.css";
 import Timer from "./Timer";
-import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div className="app flex items-center justify-center h-screen text-center">
-        <Timer />
-      </div>
-    </BrowserRouter>
+    <div className="app flex items-center justify-center h-screen text-center">
+      <Timer />
+    </div>
   );
 }
